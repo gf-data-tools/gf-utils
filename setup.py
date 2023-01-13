5e1273025d276b13fe95fcb04f6f0047c9b5eef0
ZeroRin <pptxp@qq.com>
2023-01-13 22:54:39 +0800

    remove unitypy

--- setup.py
+++ setup.py
@@ -1,16 +1,15 @@
 from setuptools import setup
 
 setup(
-   name='gf-utils',
-   version='1.0',
-   description='A module includes various tools relates to the game girls frontline',
-   author='ZeroRin',
-   author_email='pptxp@qq.com',
-   packages=['gf_utils'], 
-   install_requires=[
-         'tqdm',
-         'UnityPy',
-         'pycryptodome',
-         'logger_tt'
-   ],
+    name="gf-utils",
+    version="1.0",
+    description="A module includes various tools relates to the game girls frontline",
+    author="ZeroRin",
+    author_email="pptxp@qq.com",
+    packages=["gf_utils"],
+    install_requires=[
+        "tqdm",
+        "pycryptodome",
+        "logger_tt",
+    ],
 )
